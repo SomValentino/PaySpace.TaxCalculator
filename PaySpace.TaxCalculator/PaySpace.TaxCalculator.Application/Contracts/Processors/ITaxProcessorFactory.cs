7387a20307859cb64913c1656ee141944e98b01c
SomValentino <49089500+SomValentino@users.noreply.github.com>
2023-05-19 17:54:55 +0200

    Added TaxProcessor Factory

---
+++ PaySpace.TaxCalculator/PaySpace.TaxCalculator.Application/Contracts/Processors/ITaxProcessorFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaySpace.TaxCalculator.Application.Contracts.Processors
+{
+    internal class ITaxProcessorFactory
+    {
+    }
+}
